2fb305df2484b8eb72da6bee21e549bd6c0b63b4
James534 <james534@outlook.com>
2015-09-19 15:27:22 -0400

    Added responce to the server. The server is now sending back whatever you type, and if you uncomment the sendMail function, it sends a stock email in parts to the phone

--- Server/server.py
+++ Server/server.py
@@ -2,19 +2,23 @@ from flask import Flask, request, redirect
 import twilio.twiml
 from twilio.rest import TwilioRestClient
  
+MAX_CHARS = 1550
 class Etxt_server():
 
 	def setup(self):
 		f = open('data.txt', 'r')
-		#self.url            = f.readline().strip('\n')
-		#self.webController  = f.readline().strip('\n')
 		self.sid = f.readline().strip('\n')
 		self.auth = f.readline().strip('\n')
 		self.clientNumber = f.readline().strip('\n')
 		self.serverNumber = f.readline().strip('\n')
 		self.client = TwilioRestClient(self.sid, self.auth)
-		#print(self.url)
+		self.startingHash = f.readline().strip('\n')
+		self.endingHash   = f.readline().strip('\n')
+		#debug stuff
+		self.email = open("sampleEmail.txt", 'r').read()
+		print (len(self.email))
 
+	#responces have to be less than 1562 characters
 	def text(self, msg):
 		message = self.client.messages.create(
 			body="|" + msg,
@@ -24,26 +28,72 @@ class Etxt_server():
 		print ('texting', msg)
 		#print (msg)
 
+	def sendMail(self, msg):
+		if len(msg)+10 > MAX_CHARS:
+			brokenMsg = []									#to hold all the broken down messages
+
+			index = len(msg)/MAX_CHARS + 1
+			maxIndex = index
+			if (maxIndex < 10):
+				strMaxIndex = '0' + str(maxIndex)
+			else:
+				strMaxIndex = str(maxIndex)
+			#firstMsg = self.startingHash + msg[:MAX_CHARS]
+			#brokenMsg.append(firstMsg)
+			#index -= 1
+			wordIndex = 0
+
+			for i in range (1, index+1):
+				if (i < 10):
+					counter = '0' + str(i) + '/' + strMaxIndex
+				else:
+					counter + str(i) + '/' + strMaxIndex
+				brokenMsg.append(counter + msg[wordIndex:wordIndex+MAX_CHARS])
+				wordIndex += MAX_CHARS
+
+			#index = MAX_CHARS-6								#since im taking max_chars-6 characters in the first message, make the index max_chars-6
+			#length = len(msg) + 10 - MAX_CHARS-6			#current length of the email
+			#words = ES.startingHash + msg[0:MAX_CHARS-6]	#the first message to be sent
+			#brokenMsg.append(words)							#add the first message to the list 
+#
+			#for x in range (0, (len(msg)+10)/MAX_CHARS):		#+6 because 1 from the | character and 5 from the possible start/end hash
+				#brokenMsg.append(msg[index:index+MAX_CHARS])
+				#print(msg[index:index+MAX_CHARS])
+				#index += MAX_CHARS
+
+			#sending the messages after its all broken down
+			for w in brokenMsg:
+				self.text(w)
+		else:
+			self.text(ES.startingHash + ES.endingHash + msg)
+		print ("EOF")
+
 ES = Etxt_server()
 ES.setup()
+email = open("sampleEmail.txt", 'r').read()
+words = "WASDSADSADSADAS"
 #ES.text("HELLO WORLD")
 #print("EOF")
 
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
-    resp = twilio.twiml.Response()
+    """Respond to incoming texts"""
+    #resp = twilio.twiml.Response()
     rq = ES.client.messages.list();
-    print ("MESSAGE")
+    print ("____MESSAGE____")
     print(rq[0].body)
     print(len(rq))
     #for x in range (0,5):
     #	print(rq[x].body)
     #for message in rq:
-    #	print (message.body)
-    resp.message(rq[0].body)
-    return str(resp)
+    #	print (message.body)	
+    #resp.message(rq[0].body)
+    #resp.message(email)
+    #return str(resp)
+    ES.text(rq[0].body)
+    #ES.sendMail(email)
+    return "eof"
  
 if __name__ == "__main__":
     app.run(debug=True)
